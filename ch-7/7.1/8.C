b5c64762ca45a691c5ade00eb208388abbc6f004
Jashgolakiya877 <jashgolakiya877@gmail.com>
2023-05-22 17:21:19 +0530

    ch7

---
+++ ch-7/7.1/8.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+ int num=5;
+ int a,b;
+ clrscr();
+ for(a=1;a<=num;a++)
+ {
+	 for(b=1;b>=a;b++)
+	 {
+	printf("%d",rem);
+	}
+	printf("\n");
+
+ }
+
+ getch();
+}
